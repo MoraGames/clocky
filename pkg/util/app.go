2b60f7d8d37f750774592cf40e1d10551ac37996
MoraGames <andreamorandi2612@gmail.com>
2023-12-11 15:56:27 +0100

    (feat) Add app infos in AppUtils data struct.

--- pkg/util/app.go
+++ pkg/util/app.go
@@ -1,10 +1,12 @@
 package util
 
 import (
+	"github.com/MoraGames/clockyuwu/config"
 	"github.com/sirupsen/logrus"
 )
 
 type AppUtils struct {
+	ConfigApp  config.App
 	Logger     *logrus.Logger
 	TimeFormat string
 }
