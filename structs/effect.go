f9ba6d5ed0c93b796054072051264a35bece76d4
MoraGames <andreamorandi2612@gmail.com>
2023-12-29 00:06:20 +0100

    (feat) Implements EffectPresence data structure. Add static variables for all the possible effects.

--- structs/effect.go
+++ structs/effect.go
@@ -1,8 +1,39 @@
 package structs
 
+import "github.com/MoraGames/clockyuwu/pkg/types"
+
 type Effect struct {
 	Name  string
 	Scope string
 	Key   string
 	Value int
 }
+
+type EffectPresence struct {
+	Effect   *Effect
+	Possible float64
+	Amount   types.Interval
+}
+
+var (
+	// Multiplier
+	TripleNegativePoints    = &Effect{"Mul -3", "Event", "*", -3}
+	DoubleNegativePoints    = &Effect{"Mul -2", "Event", "*", -2}
+	SingleNegativePoints    = &Effect{"Mul -1", "Event", "*", -1}
+	DoublePositivePoints    = &Effect{"Mul +2", "Event", "*", 2}
+	TriplePositivePoints    = &Effect{"Mul +3", "Event", "*", 3}
+	QuintuplePositivePoints = &Effect{"Mul +5", "Event", "*", 5}
+
+	// Additive
+	SubTwoPoints   = &Effect{"Sub 2", "Event", "-", 2}
+	SubOnePoint    = &Effect{"Sub 1", "Event", "-", 1}
+	AddOnePoint    = &Effect{"Add 1", "Event", "+", 1}
+	AddTwoPoints   = &Effect{"Add 2", "Event", "+", 2}
+	AddThreePoints = &Effect{"Add 3", "Event", "+", 3}
+
+	// Special Effects
+	ComebackBonus1  = &Effect{"Comeback 1", "User", "+", 1}
+	ComebackBonus2  = &Effect{"Comeback 2", "User", "+", 2}
+	ComebackBonus3  = &Effect{"Comeback 3", "User", "+", 3}
+	LastChanceBonus = &Effect{"Last Chance", "User", "+", 2}
+)
