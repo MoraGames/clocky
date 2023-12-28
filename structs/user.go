1359840eb9ac2e4a317e854324cc60e933181023
MoraGames <andreamorandi2612@gmail.com>
2023-12-29 00:05:04 +0100

    (feat) Add TelegramID property. Edit NewUser() function. Implements AddEffects() and RemoveEffects() functions.

--- structs/user.go
+++ structs/user.go
@@ -1,15 +1,32 @@
 package structs
 
 type User struct {
+	TelegramID                      int64
 	UserName                        string
 	TotalPoints                     int
 	TotalEventPartecipations        int
 	TotalEventWins                  int
 	TotalChampionshipPartecipations int
 	TotalChampionshipWins           int
-	Effects                         []Effect
+	Effects                         []*Effect
 }
 
-func NewUser(username string) *User {
-	return &User{username, 0, 0, 0, 0, 0, make([]Effect, 0)}
+func NewUser(telegramID int64, username string) *User {
+	return &User{telegramID, username, 0, 0, 0, 0, 0, make([]*Effect, 0)}
+}
+
+func (u *User) AddEffects(effects ...*Effect) {
+	u.Effects = append(u.Effects, effects...)
+}
+
+func (u *User) RemoveEffects(effects ...*Effect) {
+	newUserEffect := make([]*Effect, 0)
+	for _, e := range u.Effects {
+		for _, effect := range effects {
+			if e != effect {
+				newUserEffect = append(newUserEffect, e)
+			}
+		}
+	}
+	u.Effects = newUserEffect
 }
