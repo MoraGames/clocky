fc659ccf340aad4bd050d944e6a5483498cd9c69
MoraGames <andreamorandi2612@gmail.com>
2024-03-30 23:43:32 +0100

    (fix) Update old key parameter type from string to int64.

--- repo/repoer.go
+++ repo/repoer.go
@@ -20,15 +20,15 @@ type EffectRepoer interface {
 	Create(*model.Effect) (int64, error)
 	Get(int64) (*model.Effect, error)
 	GetAll() []*model.Effect
-	Update(string, *model.Effect) error
-	Delete(string) error
+	Update(int64, *model.Effect) error
+	Delete(int64) error
 }
 type EventRepoer interface {
 	Create(*model.Event) (int64, error)
 	Get(int64) (*model.Event, error)
 	GetAll() []*model.Event
-	Update(string, *model.Event) error
-	Delete(string) error
+	Update(int64, *model.Event) error
+	Delete(int64) error
 }
 type EventInstanceReport interface {
 	Create(*model.EventInstance) (int64, error)
@@ -48,8 +48,8 @@ type RecordRepoer interface {
 	Create(*model.Record) (int64, error)
 	Get(int64) (*model.Record, error)
 	GetAll() []*model.Record
-	Update(string, *model.Record) error
-	Delete(string) error
+	Update(int64, *model.Record) error
+	Delete(int64) error
 }
 type SetRepoer interface {
 	Create(*model.Set) (int64, error)
