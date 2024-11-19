7d65661e3022b787a066fc3ca15ddcce387ed638
MoraGames <andreamorandi2612@gmail.com>
2024-11-19 15:58:21 +0100

    (refactor) Merge branch master

---
+++ pkg/utils/application.go
@@ -0,0 +1,14 @@
+package utils
+
+import (
+	"github.com/MoraGames/clockyuwu/config"
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"github.com/sirupsen/logrus"
+)
+
+type Application struct {
+	Config  *config.Config
+	Logger  *logrus.Logger
+	BotAPI  *tgbotapi.BotAPI
+	Updates tgbotapi.UpdatesChannel
+}
