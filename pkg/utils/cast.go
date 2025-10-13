902a481231f9132339808f67f7a226d62e5437ba
MoraGames <andreamorandi2612@gmail.com>
2025-10-14 01:11:38 +0200

    (feat) Add bool to int utils function.

--- pkg/utils/cast.go
+++ pkg/utils/cast.go
@@ -1,5 +1,19 @@
 package utils
 
+func ItoB(i int) bool {
+	if i == 0 {
+		return false
+	}
+	return true
+}
+
+func BtoI(b bool) int {
+	if !b {
+		return 0
+	}
+	return 1
+}
+
 func BtoS(b bool) string {
 	if b {
 		return "true"
