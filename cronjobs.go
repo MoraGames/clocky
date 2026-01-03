f14ab7c9699195f8aef10ff2e738d07a06c23b57
MoraGames <andreamorandi2612@gmail.com>
2026-01-03 01:32:01 +0100

    (fix) Correct /bonus message comunication about next level partecipation unlock requirments.

--- cronjobs.go
+++ cronjobs.go
@@ -291,7 +291,7 @@ func DailyUserRewardAndReset(users map[int64]*structs.User, dailyEnabledEvents i
 				todayRewardedUsers = append(todayRewardedUsers, events.DailyRewardedUser{User: user.Minimize(), Sets: choosenSets})
 
 				// Check if the user has won at least [90/75/60]% (based on the level) of the events in which he participated, if so increase his activity streak
-				if user.DailyEventWins >= int(math.Round(float64(user.DailyEventPartecipations)*(1.00-(float64(level)*0.15)))) {
+				if user.DailyEventWins >= int(math.Round(float64(user.DailyEventPartecipations)*(1.05-(float64(level)*0.15)))) {
 					user.DailyActivityStreak++
 				} else {
 					user.DailyActivityStreak = 0
